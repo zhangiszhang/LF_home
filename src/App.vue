c82a3f9bb4a5bb4e31cdceeea600203711db9cc7
binbinxiha <1154041905@qq.com>
2017-09-18 15:26:03 +0800

    wu

--- src/App.vue
+++ src/App.vue
@@ -4,76 +4,127 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
-</script>
+<script>export default {
+	name: 'app'
+}</script>
 
-<style> 
-body,p,dl,dt,dd,ul,li,ol,fieldset,legend,h1,h2,h3,h4,h5,h6{margin:0;padding:0;}
-body,html{height:100%;font-family:"\5FAE\8F6F\96C5\9ED1";}
-img,iframe{border:none}
-a{text-decoration:none;color:#333;}
-ul,li,ol{list-style:none}
-.clear{width:0;height:0;overflow:hidden;clear:both;}@media screen and (max-width: 1602px) {
-  html {
-    font-size: 250px;
-  }
-}
-@media screen and (max-width: 1026px) {
-  html {
-    font-size: 160px;
-  }
-}
-@media screen and (max-width: 802px) {
-  html {
-    font-size: 125px;
-  }
-}
-@media screen and (max-width: 770px) {
-  html {
-    font-size: 120px;
-  }
-}
-@media screen and (max-width: 642px) {
-  html {
-    font-size: 100px;
-  }
-}
-@media screen and (max-width: 602px) {
-  html {
-    font-size: 93.75px;
-  }
-}
-@media screen and (max-width: 416px) {
-  html {
-    font-size: 64.6875px;
-  }
-}
-@media screen and (max-width: 414px) {
-  html {
-    font-size: 64.375px;
-  }
-}
-@media screen and (max-width: 386px) {
-  html {
-    font-size: 60px;
-  }
-}
-@media screen and (max-width: 377px) {
-  html {
-    font-size: 58.59375px;
-  }
-}
-@media screen and (max-width: 362px) {
-  html {
-    font-size: 56.25px;
-  }
-}
-@media screen and (max-width: 322px) {
-  html {
-    font-size: 50px;
-  }
-}
-</style>
+<style> body,
+ p,
+ dl,
+ dt,
+ dd,
+ ul,
+ li,
+ ol,
+ fieldset,
+ legend,
+ h1,
+ h2,
+ h3,
+ h4,
+ h5,
+ h6 {
+ 	margin: 0;
+ 	padding: 0;
+ }
+ 
+ body,
+ html {
+ 	height: 100%;
+ 	font-family: "\5FAE\8F6F\96C5\9ED1";
+ }
+ 
+ img,
+ iframe {
+ 	border: none
+ }
+ 
+ a {
+ 	text-decoration: none;
+ 	color: #333;
+ }
+ 
+ ul,
+ li,
+ ol {
+ 	list-style: none
+ }
+ 
+ .clear {
+ 	width: 0;
+ 	height: 0;
+ 	overflow: hidden;
+ 	clear: both;
+ }
+ 
+ @media screen and (max-width: 1602px) {
+ 	html {
+ 		font-size: 250px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 1026px) {
+ 	html {
+ 		font-size: 160px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 802px) {
+ 	html {
+ 		font-size: 125px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 770px) {
+ 	html {
+ 		font-size: 120px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 642px) {
+ 	html {
+ 		font-size: 100px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 602px) {
+ 	html {
+ 		font-size: 93.75px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 416px) {
+ 	html {
+ 		font-size: 64.6875px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 414px) {
+ 	html {
+ 		font-size: 64.375px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 386px) {
+ 	html {
+ 		font-size: 60px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 377px) {
+ 	html {
+ 		font-size: 58.59375px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 362px) {
+ 	html {
+ 		font-size: 56.25px;
+ 	}
+ }
+ 
+ @media screen and (max-width: 322px) {
+ 	html {
+ 		font-size: 50px;
+ 	}
+ }</style>
